b329f1f6471c9e6409d023f1e2b9ececdfac48bd
Destinum <david.hoglund.96@gmail.com>
2022-08-01 05:20:39 +0200

    Test

--- data/moves.ts
+++ data/moves.ts
@@ -19879,7 +19879,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (this.randomChance(1, 1)) {
 				move.target = "self";
 				move.accuracy = true;
-				this.add('-start', pokemon, 'Unstable Concoction');
+				this.add('-end', pokemon, 'Unstable Concoction');
 			}	
 		},
 		secondary: null,
