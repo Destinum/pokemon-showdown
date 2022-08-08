96ab0cf1194caa7eb1dfc71971b9181be576e2dd
Destinum <david.hoglund.96@gmail.com>
2022-08-08 23:12:26 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -20147,7 +20147,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		flags: {contact: 1, protect: 1, mirror: 1},
 		secondary: null,
 		target: "normal",
-		type: "Water",
+		type: "Flying",
 		contestType: "Tough",
 	},
 };
