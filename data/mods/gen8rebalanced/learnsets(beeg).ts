32324d303f6ae5bd6645ead5b50651ed50e791d0
Destinum <david.hoglund.96@gmail.com>
2023-03-15 22:08:53 +0100

    zz

--- data/mods/gen8rebalanced/learnsets(beeg).ts
+++ data/mods/gen8rebalanced/learnsets(beeg).ts
@@ -34569,7 +34569,7 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			uproar: ["8M", "7T", "6T", "5T", "4T"],
 			waterpulse: ["7T", "6T", "4M", "3M"],
 			whirlpool: ["8M", "4M"],
-			//recover: ["8M"],			//Custom Move, added for Nathan
+			recover: ["8M"],			//Custom Move, added for Nathan
 		},
 		eventData: [
 			{generation: 3, level: 100, moves: ["irontail", "protect", "metalsound", "doubleedge"], pokeball: "pokeball"},
